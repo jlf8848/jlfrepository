353925158dce951ecde58972665eabd144128ba6
jlf8848 <304813288@qq.com>
2017-11-28 22:32:55 +0800

    add helloworld.java

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+package jlf;
+
+public class HelloWorld {
+    public HelloWorld() {
+    }
+    public static void main(String[] args)
+    {
+        int a1=5;
+        int b1=9;
+        int c1;
+        c1=a1+b1;
+        function1();
+
+
+
+        System.out.printf("HelloWorld");
+    }
+    public  static void function1()
+    {
+        int a2=5;
+        int b2=9;
+        int c2;
+        c2=a2+b2;
+
+        System.out.printf("test\n");
+    }
+}
